a8ee1db930833013087611820093778b2be80486
sebazac332 <seb55381@hotmail.com>
2025-11-28 15:22:33 -0500

    teste para problema de CORS

--- app/main.py
+++ app/main.py
@@ -11,11 +11,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["*"],
 )
 
 app.include_router(auth_router)
