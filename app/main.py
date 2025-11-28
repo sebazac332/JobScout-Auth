c5eca738c7b163cc16228b8b58ce61f1826755fc
sebazac332 <seb55381@hotmail.com>
2025-11-28 16:10:23 -0500

    tentativa de solventar erro de CORS

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Authentication API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="https://jobscout-frontend-production.up.railway.app",
+    allow_origins=["https://jobscout-frontend-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
