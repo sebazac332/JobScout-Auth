c5e256f6c2f9605bea4bd967100ca790039a34a3
sebazac332 <seb55381@hotmail.com>
2025-11-28 15:29:24 -0500

    trocada url de CORS

--- app/main.py
+++ app/main.py
@@ -5,13 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Authentication API")
 
 origins = [
-    "http://localhost:3000",
-    "https://jobscout-frontend-production.up.railway.app",
+   "https://jobscout-frontend-production.up.railway.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
